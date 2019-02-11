a4496a74c6d572fa5d85eb32cdc1de3ef28be7c5
YvonneNeuland <YvonneNeuland@fullsail.edu>
2019-02-11 17:12:42 -0500

    Added navbar

--- pokemon-demo-app/src/app/components/components.module.ts
+++ pokemon-demo-app/src/app/components/components.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
-  declarations: []
+  declarations: [
+    NavbarComponent,
+    LogoComponent,
+  ],
+  exports: [
+    NavbarComponent,
+    LogoComponent,
+  ]
 })
 export class ComponentsModule { }
--- pokemon-demo-app/src/app/pages/pages.module.ts
+++ pokemon-demo-app/src/app/pages/pages.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '../components/components.module';
 import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule,
   ],
-  declarations: [HomePageComponent]
+  declarations: [
+    HomePageComponent,
+  ]
 })
 export class PagesModule { }
